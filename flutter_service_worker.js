9e1a7ccff0e4f9d358ce93878191bd1e6638e06c
Federico Amura <fede.amura@gmail.com>
2021-12-02 01:34:03 -0300

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "11a08516861d2d0370b91363d91e5c80",
-"index.html": "abbdd532540baef1ae2b4a12cdd2d50b",
-"": "abbdd532540baef1ae2b4a12cdd2d50b",
-"main.dart.js": "bdb4c5c8b18ed0499f6504ba28306409",
+"index.html": "1ee84307072362812b9ec53fe83ff13a",
+"": "1ee84307072362812b9ec53fe83ff13a",
+"main.dart.js": "e5f35e94d298b4b17e48f6147212dd02",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
